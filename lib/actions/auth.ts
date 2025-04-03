70fbd6684f246c0da447a08b6c8e8e1d4dcc5473
xenon <pmcruz@hermenautas.com>
2025-04-03 22:11:44 +0200

    fix error form SignUp

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,7 +8,7 @@ import { signIn } from "@/auth";
 import { headers } from "next/headers";
 import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
-import { workflowClient } from "../workflow";
+import { workflowClient } from "@/lib/workflow";
 import config from "@/lib/config";
 
 export const signInWithCredentials = async (
@@ -73,7 +73,7 @@ export const signUp = async (params: AuthCredentials) => {
       body: {
         email,
         fullName,
-      }
+      },
     });
 
     await signInWithCredentials({ email, password });
